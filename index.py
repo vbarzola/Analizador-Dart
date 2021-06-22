ed476c2dcd15e3b3520931ddbd9fa5948362410a
Valeria Barzola <valeria.barzola@espol.edu.ec>
2021-06-21 22:27:00 -0500

    Primer commit palabras reservadas

---
+++ index.py
@@ -0,0 +1,34 @@
+from ply import lex
+"""
+Aporte valeria
+"""
+reservadas = {
+    'bool': 'BOOL',
+    'break': 'BREAK',
+    'const': 'CONST',
+    'continue': 'CONTINUE',
+    'double': 'DOUBLE',
+    'else': 'ELSE',
+    'false': 'FALSE',
+    'final': 'FINAL',
+    'for': 'FOR',
+    'function': 'FUNCTION',
+    'if': 'IF',
+    'import': 'IMPORT',
+    'in': 'IN',
+    'int': 'INT',
+    'List': 'LIST',
+    'Map': 'MAP',
+    'null': 'NULL',
+    'print': 'PRINT',
+    'readLineSync': 'READ_LINE_SYNC',
+    'return': 'RETURN',
+    'Set': 'SET',
+    'stdin': 'STDIN',
+    'String': 'STRING',
+    'true': 'TRUE',
+    'var': 'VAR',
+    'void': 'VOID',
+    'while': 'WHILE'
+}
+
