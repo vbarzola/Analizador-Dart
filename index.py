86c45e0cda69429999a9233e236e89532295881a
Valeria Barzola <valeria.barzola@espol.edu.ec>
2021-06-21 22:30:19 -0500

    definiendo expresiones regulares

--- index.py
+++ index.py
@@ -44,3 +44,41 @@ tokens = ["ID", "NUM_ENTERO", "NUM_DECIMAL", "CADENA_CARAC",
           "MAYOR_QUE", "MENOR_QUE", "IGUAL_QUE", "DIFERENTE_QUE", "MENOR_IGUAL", "MAYOR_IGUAL",
           "NEGACION", "Y", "O"
           ] + list(reservadas.values())
+
+"""
+Aporte valeria
+"""
+t_NUM_ENTERO = r'([1-9]\d+|\d)'
+t_NUM_DECIMAL = r'([1-9]\d+|\d)\.\d+'
+t_CADENA_CARAC = r'(\'[^\'\n]*\'|"[^"\n]*")'
+t_IPAR = r'\('
+t_DPAR = r'\)'
+t_ICORCH = r'\['
+t_DCORCH = r'\]'
+t_ILLAVE = r'\{'
+t_DLLAVE = r'\}'
+t_ASIGNAR = r'='
+t_PUNTO_COMA = r';'
+t_PUNTO = r'\.'
+t_COMA = r','
+t_DOS_PUNTOS = r':'
+t_MAS = r'\+'
+t_MENOS = r'\-'
+t_POR = r'\*'
+t_DIVIDIDO = r'\/'
+t_DIVISION_ENTERA = r'~\/'
+t_MODULO = r'%'
+t_AUTOINCREMENTO = r'\+\+'
+t_AUTODECREMENTO = r'\-\-'
+t_MAYOR_QUE = r'>'
+t_MENOR_QUE = r'<'
+t_IGUAL_QUE = r'=='
+t_DIFERENTE_QUE = r'!='
+t_MENOR_IGUAL = r'<='
+t_MAYOR_IGUAL = r'>='
+t_NEGACION = r'!'
+t_Y = r'&&'
+t_O = r'\|\|'
+
+
+t_ignore = " \t"
